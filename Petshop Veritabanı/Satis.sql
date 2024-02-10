a4ee5f0b3201434745bb0ad886b07f06c6346f6e
Yaren <154266602+Yaren-Akarsu@users.noreply.github.com>
2024-02-10 16:46:08 +0300

    Petshop Veritabanı ve Web Sitesi

---
+++ Petshop Veritabanı/Satis.sql
@@ -0,0 +1,13 @@
+insert into Satis (satis_no, hayvan_no, mama_no, alan_no, kiyafet_aksesuar_no, bakim_esyasi_no, oyuncak_no, satis_miktari, satis_tarihi, sube_no) values (5,1,2,1,3,13,23,1,'2023-11-1 13:32:00',4);
+insert into Satis (satis_no, hayvan_no, mama_no, alan_no, kiyafet_aksesuar_no, bakim_esyasi_no, oyuncak_no, satis_miktari, satis_tarihi, sube_no) values (10,2,4,3,6,12,19,2,'2023-11-2 15:20:00',8);
+insert into Satis (satis_no, hayvan_no, mama_no, alan_no, kiyafet_aksesuar_no, bakim_esyasi_no, oyuncak_no, satis_miktari, satis_tarihi, sube_no) values (16, 3, 6, 5, 9, 11, 18, 3, '2023-11-03 09:05:00', 12);
+insert into Satis (satis_no, hayvan_no, mama_no, alan_no, kiyafet_aksesuar_no, bakim_esyasi_no, oyuncak_no, satis_miktari, satis_tarihi, sube_no) values (21, 4, 8, 7, 12, 10, 17, 3, '2023-11-04 11:30:00', 16);
+insert into Satis (satis_no, hayvan_no, mama_no, alan_no, kiyafet_aksesuar_no, bakim_esyasi_no, oyuncak_no, satis_miktari, satis_tarihi, sube_no) values (26, 5, 10, 9, 15, 9, 16, 2, '2023-11-05 13:13:00', 20);
+insert into Satis (satis_no, hayvan_no, mama_no, alan_no, kiyafet_aksesuar_no, bakim_esyasi_no, oyuncak_no, satis_miktari, satis_tarihi, sube_no) values (31, 6, 12, 11, 18, 8, 15, 1, '2023-11-06 15:30:00', 24);
+insert into Satis (satis_no, hayvan_no, mama_no, alan_no, kiyafet_aksesuar_no, bakim_esyasi_no, oyuncak_no, satis_miktari, satis_tarihi, sube_no) values (36, 7, 14, 13, 21, 7, 14, 1, '2023-11-07 17:30:00', 28);
+insert into Satis (satis_no, hayvan_no, mama_no, alan_no, kiyafet_aksesuar_no, bakim_esyasi_no, oyuncak_no, satis_miktari, satis_tarihi, sube_no) values (41, 8, 16, 15, 24, 6, 13, 2, '2023-11-08 18:18:00', 32);
+insert into Satis (satis_no, hayvan_no, mama_no, alan_no, kiyafet_aksesuar_no, bakim_esyasi_no, oyuncak_no, satis_miktari, satis_tarihi, sube_no) values (46, 9, 18, 17, 27, 5, 12, 3, '2023-11-09 10:30:00', 36);
+insert into Satis (satis_no, hayvan_no, mama_no, alan_no, kiyafet_aksesuar_no, bakim_esyasi_no, oyuncak_no, satis_miktari, satis_tarihi, sube_no) values (51, 10, 20, 19, 30, 4, 12, 3, '2023-11-10 09:06:00', 40);
+insert into Satis (satis_no, hayvan_no, mama_no, alan_no, kiyafet_aksesuar_no, bakim_esyasi_no, oyuncak_no, satis_miktari, satis_tarihi, sube_no) values (56, 11, 22, 21, 33, 3, 19, 2, '2023-11-11 16:30:00', 44);
+insert into Satis (satis_no, hayvan_no, mama_no, alan_no, kiyafet_aksesuar_no, bakim_esyasi_no, oyuncak_no, satis_miktari, satis_tarihi, sube_no) values (61, 12, 24, 23, 36, 2, 10, 1, '2023-11-12 14:25:00', 48);
+
